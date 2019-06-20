b7cb9f2d3744a656b8f1fa34845ea20051bd62d3
Michael Keenan <michaelkeenan.com@gmail.com>
2019-06-20 10:10:36 -0400

    Create JavaScript-forEachHelper-1.js

---
+++ JavaScript-forEachHelper-1.js
@@ -0,0 +1,10 @@
+var colors = [ 'red', 'blue', 'green' ]
+var color;
+
+for (var i = 0; i < colors.length; i++) {
+console.log(color);
+}
+
+colors.forEach(function(color) {
+  console.log(color);
+});
