1f532c7a2853651c5e44aa170c4a2cfda66a2f60
Michael Keenan <michaelkeenan.com@gmail.com>
2019-06-21 09:18:02 -0400

    Create JavaScript - Map Helper - Coding Exercise - 4.js

---
+++ JavaScript - Map Helper - Coding Exercise - 4.js
@@ -0,0 +1,11 @@
+var images = [
+  { height: '34px', width: '39px' },
+  { height: '54px', width: '19px' },
+  { height: '83px', width: '75px' },
+];
+
+var tall = images.map(function(vertical) {
+  return vertical.height;
+});
+
+tall;
