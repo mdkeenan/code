71eea59db41ab995f2a8782482296afa2a470c34
Michael Keenan <michaelkeenan.com@gmail.com>
2019-07-05 12:56:04 -0400

    Create JavaScript - Find Helper - 2.js

---
+++ JavaScript - Find Helper - 2.js
@@ -0,0 +1,15 @@
+// Find car Focus
+
+function Car(model) {
+  this.model = model;
+}
+
+var cars = [
+  new Car('Buick'),
+  new Car('Camaro'),
+  new Car('Focus')
+];
+
+cars.find(function(car) {
+  return car.model === 'Focus';
+});
