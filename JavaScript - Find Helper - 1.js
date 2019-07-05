f04683b5f5d1b6593e812d6e3d6dcd2638827f0f
Michael Keenan <michaelkeenan.com@gmail.com>
2019-07-05 12:51:35 -0400

    Update JavaScript - Find Helper - 1.js

--- JavaScript - Find Helper - 1.js
+++ JavaScript - Find Helper - 1.js
@@ -1,9 +1,10 @@
-// Find username Alex
+// Find username Alex (only finds the first record)
 
 var users = [
   { name: 'Jill' },
   { name: 'Alex' },
-  { name: 'Bill' }
+  { name: 'Bill' },
+  { name: 'Alex' }
 ];
 
 users.find(function(user) {
