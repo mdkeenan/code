bce46400cf79bbb1bc37f3ee23efb5e0ddd70ebc
Michael Keenan <michaelkeenan.com@gmail.com>
2022-06-02 18:49:08 -0400

    Create Python - SimpleInputPrint.py

---
+++ Python - SimpleInputPrint.py
@@ -0,0 +1,6 @@
+birth_year = input('Birth year: ')
+print(type(birth_year))
+# Need to convert birth_year from string to integer: int(birth_year)
+age = 2022 - int(birth_year)
+print(type(age))
+print(age)
