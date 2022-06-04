4b5436f275a7a897f2e0fb641d75096da041ba35
Michael Keenan <michaelkeenan.com@gmail.com>
2022-06-04 19:36:12 -0400

    Create Python - Math Functions - 1.py

---
+++ Python - Math Functions - 1.py
@@ -0,0 +1,17 @@
+# Math Functions
+
+# Import the math module to use functions within it.
+import math
+
+# Use the round function to round a number up.
+x = 2.9
+print(round(x))
+
+# Use the absolute (abs) function to determine the absolute value of a number.
+print(abs(-2.9))
+
+# From the math module use the ceiling (ceil) method to find the next whole number of a fraction.
+print(math.ceil(3.8))
+
+# From the math module use the floor (floor) method to find the previous whole number of a fraction.
+print(math.floor(3.8))
