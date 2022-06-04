fec9f455b13e5d764ce437686462859bcb8dd83f
Michael Keenan <michaelkeenan.com@gmail.com>
2022-06-04 18:52:19 -0400

    Create Python - String Manipulation - 2.py

---
+++ Python - String Manipulation - 2.py
@@ -0,0 +1,14 @@
+# Define a variable
+course = 'Python is VERY good'
+# Print course variable content.
+print(course)
+# Use upper method to make all letters in the course variable uppercase.
+print(course.upper())
+# Use lower method to make all letters in the course variable lowercase.
+print(course.lower())
+# Use the find method to determine where the letter o is in the course variable.
+print(course.find('o'))
+# Use the replace method to replace good with bad.
+print(course.replace('good', 'bad'))
+# Determine if the string VERY is contained in the course variable.
+print('VERY' in course)
