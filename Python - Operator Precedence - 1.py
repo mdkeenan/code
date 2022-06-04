1214526281fb42fad44708dad4aef666a048af87
Michael Keenan <michaelkeenan.com@gmail.com>
2022-06-04 19:26:55 -0400

    Create Python - Operator Precedence - 1.py

---
+++ Python - Operator Precedence - 1.py
@@ -0,0 +1,7 @@
+# Operator Precedence. Like in algabra PEMDAS.
+
+# Order of Operations: Parenthesis > Exponentiation > (Multiplication or Division) > (Addition or Subraction)
+# Example: First (6 - 4 = 2) Second (2 ** 2 = 4) Third (3 * 4 = 12) Fourth (10 + 12 = 22) Fifth (22 + 2 = 24)
+
+x = (6 - 4) + 10 + 3 * 2 ** 2
+print(x)
