5bb29ac26950093ea52bdd9502dbe5222a89cca1
Michael Keenan <michaelkeenan.com@gmail.com>
2022-06-04 19:07:11 -0400

    Create Python - Arithmetic Operations - 1.py

---
+++ Python - Arithmetic Operations - 1.py
@@ -0,0 +1,16 @@
+# Determine what 10 devided by 3 is.
+print(10/3)
+
+# Determine how many times 3 fits into 10 without the remainder.
+print(10//3)
+
+# Dtermin what is left over after determining how many times 3 can go into 10.
+print(10%3)
+
+# Use an exponent to determine the value of 10 to the power of 3.
+print(10**3)
+
+# Use an augmented assignment operator to increment a value with less steps.
+x = 10
+x += 3
+print(x)
