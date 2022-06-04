31093e8e6a5f9ecdd031aec4ea6166a2e1bdbd39
Michael Keenan <michaelkeenan.com@gmail.com>
2022-06-04 19:51:20 -0400

    Create Python - If Statements - 1.py

---
+++ Python - If Statements - 1.py
@@ -0,0 +1,18 @@
+# If Statements
+
+# Use boolean logic to set is_hot and is_cold variables to True or False.
+is_hot = False
+is_cold = False
+
+# Use an if statement to determine if the variable "is_hot" is equal to True and if so print "It's a hot day", "Drink plenty of water" and "Enjoy your day".
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+# Use an elif statement to determine if the variable "is_cold" is equal to True and if so print "It's a cold day", "Wear warm clothes" and "Enjoy your day".
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+# Use an else statement so that if neither of the above conditions are true print "It's a lovely day" and "Enjoy your day".
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
