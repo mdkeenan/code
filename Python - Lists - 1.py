7ef81603e31574fc9008a79a00163ba4bbcd6318
Michael Keenan <michaelkeenan.com@gmail.com>
2022-06-11 01:20:55 -0400

    Create Python - Lists - 1.py

---
+++ Python - Lists - 1.py
@@ -0,0 +1,16 @@
+# Lists
+
+names = ['John', 'Bob', 'Mike', 'Sarah', 'Mary']
+
+# This will print the names list exactly how it looks.
+print(names)
+
+for name in names:
+    i = name
+    print(i)
+
+# This will print the first item from list names.    
+print(names[0])
+
+# This will print the 2nd to the 4th item in the list.
+print(names[2:4])
